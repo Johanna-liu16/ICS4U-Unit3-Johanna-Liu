fb46546c8c3366ca33ef3e86c7429f314f222d09
Johanna Liu <johanna.liu@mths.ca>
2024-04-24 09:08:41 -0400

    Typescript done

--- Unit3-01/Typescript/index.ts
+++ Unit3-01/Typescript/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+/**
+ *
+ * This program uses stacks
+ *
+ * By:      Johanna Liu
+ * Version: 1.0
+ * Since:   2024-03-19
+ */
+
+// function reverses string
+function reverseString(stringToReverse: string): string {
+    if (stringToReverse == "") {
+        return ""
+    } else {
+        let firstChar = stringToReverse.charAt(0)
+        let restOfString = stringToReverse.slice(1, stringToReverse.length)
+        return reverseString(restOfString) + firstChar
+    }
+}
+
+const aString = "recusrsion"
+
+console.log("")
+console.log(`The original string is ${aString}`)
+let reversedString = reverseString(aString)
+console.log(`The reversed string is ${reversedString}`)
+
+console.log(`\nDone.`)
